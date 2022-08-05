d81c1acf3c2a8f69f9aa1a58e1fa423a811e7703
Anshika Agrawal <workwithanshika@gmail.com>
2022-08-05 17:15:39 +0530

    SToring data into DB

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,12 @@ const connectToMongo = require('./db');
 const express = require('express')
 connectToMongo();
 
-
 const app = express()
 const port = 3000
 
+//this line will allow us to send body in request
+app.use(express.json())
 //Available routes
-
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const user1 = require('../models/user');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    obj = {
-        name : 'Anshika',
-        gender : 'female'
-    }
-    res.json(obj)
+//Create a user using POST : /api/auth
+router.post('/', (req, res) => {
+    const user = user1(req.body);
+    user.save();
+    res.send(req.body)
 })
 
 module.exports = router
