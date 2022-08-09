116cc1f5df15ece995d415fe18ba874d5a478ad8
Anshika Agrawal <workwithanshika@gmail.com>
2022-08-09 14:31:32 +0530

    Create Add note and fetch note apis

--- backend/models/Notes.js
+++ backend/models/Notes.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 const NotesSchema = new Schema({
+    user : {
+        //foreign key
+        type : mongoose.Schema.Types.ObjectId,
+        //name of schema : copied notes from user.js 
+        ref : 'user'
+    },
     title : {
         type: String,
         required : true
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,11 +1,44 @@
 const express = require('express');
 const router = express.Router();
+var fetchuser = require('../middleware/fetchuser');
+const notes = require('../models/notes');
+const { body, validationResult } = require('express-validator');
 
-router.get('/', (req, res) => {
-    obj = {
-        name : 'Notes'
+//Route 1 : Fetch all notes of user :  GET : /api/notes/fetchallnotes : It is use to create a user
+router.get('/fetchallnotes',fetchuser, async (req, res) => {
+    
+    const fetchNotes = await notes.find({user : req.user.id});
+    res.json(fetchNotes);
+})
+
+//route 2 : Add new note using POST : /api/notes/addnote
+router.get('/addnote',fetchuser,[
+    body('title','Enter a valid Title of minimum length : 3').isLength({ min: 3 }),
+    body('description','Enter a Description of minimum length : 5').isLength({ min: 5 })
+], async (req, res) => {
+    try 
+    {
+        const errors = validationResult(req);
+        //if error comes from validation then send error and fail request
+        if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+        }
+
+        const {title, description, tag} = req.body;
+
+        const note = new notes({
+            title, description, tag, user : req.user.id
+        })
+
+        const savedNoteId = await note.save();
+        res.json(savedNoteId);
+        
+    } catch(error)
+    {
+        console.log(error.message);
+        res.status(500).send("Internal Server Error");
     }
-    res.json(obj);
+    
 })
 
 module.exports = router
