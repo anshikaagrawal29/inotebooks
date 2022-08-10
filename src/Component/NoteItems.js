5874bda4934e21ce93e6455145ce3e5f223b144a
Anshika Agrawal <workwithanshika@gmail.com>
2022-08-10 20:45:44 +0530

    Added font awesome icon 62 video

--- src/Component/NoteItems.js
+++ src/Component/NoteItems.js
@@ -7,6 +7,8 @@ export default function NoteItems( props) {
             <div className="card-body">
                 <h5 className="card-title">{props.note.title}</h5>
                 <p className="card-text">{props.note.description}</p>
+                <i class="fa-solid fa-pen-to-square mx-2"></i>
+                <i class="fa-solid fa-trash-can"></i>
             </div>
         </div>
     </div>
