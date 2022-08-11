9df13c41e904bd165f6b8918500d4eb351fcc84c
Anshika Agrawal <workwithanshika@gmail.com>
2022-08-11 17:15:25 +0530

    Added Delete api

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -4,7 +4,6 @@ import NoteContext from "./noteContext";
 const NoteState = (props) =>
 {
       const host = "http://localhost:5000";
-
       const[notes, setNotes] = useState([]);
 
       //fetch all notes
@@ -22,19 +21,25 @@ const NoteState = (props) =>
       }
 
       //Add Note
-      const addNote = (title, description, tag) =>
+      const addNote = async (title, description, tag) =>
       {
-        console.log("Adding a new note");
-        const note = {
-            "_id": "62f23b8e44dsd9ccd5a817e198a",
-            "user": "62f0c545dsd65007cb2f2dbc7bb",
-            "title": title,
-            "description": description,
-            "tag": tag,
-            "date": "2022-08-09T10:48:46.057Z",
-            "__v": 0
-          };
-            setNotes(notes.concat(note))
+        const data = JSON.stringify({
+          "title" : title,
+          "description" : description,
+          "tag" : tag
+        });
+        const url = `${host}/api/notes/addnote`;
+        const addNoteInDB = await fetch(url, {
+          method : 'POST',
+          headers : {
+            'Content-Type' : 'application/json',
+            "auth-token" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjJmMGM1NDU2NTAwN2NiMmYyZGJjN2JiIn0sImlhdCI6MTY1OTk0NjMzMX0.LgXNcHxvgqibZwWRGW_-yWsnvSq1XCBtjT2ToZtX9Mo"
+          },
+          body : data
+        });
+
+        let note = await addNoteInDB.json()
+        setNotes(notes.concat(note));
       }
 
       //Edit Note
@@ -44,10 +49,22 @@ const NoteState = (props) =>
       }
 
       //Delete Note
-      const deleteNote = (id) =>
+      const deleteNote = async (id) =>
       {
-        console.log(id);
+          const url = `${host}/api/notes/deletenote/${id}`;
+          console.log(id);
+          console.log(url);
+          const deleteNotes = await fetch(url, {
+            method : 'DELETE',
+            headers : {
+              'Content-Type' : 'application/json',
+              "auth-token" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjJmMGM1NDU2NTAwN2NiMmYyZGJjN2JiIn0sImlhdCI6MTY1OTk0NjMzMX0.LgXNcHxvgqibZwWRGW_-yWsnvSq1XCBtjT2ToZtX9Mo"
+            }
+          });
+          let note = await deleteNotes.json()
+          setNotes(notes);
       }
+
     return(
         <NoteContext.Provider value={{notes,addNote, editNote, deleteNote,fetchAllNotes}}>
             {props.children}
